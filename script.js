45523cccbb2f07eb4f28ac7d34c433187030b3ea
Manuel R. Fungate Jr. <manuelfungatejr@gmail.com>
2025-11-20 20:52:18 +0200

    I really dont know this stuff

--- script.js
+++ script.js
@@ -47,3 +47,79 @@ function updateProgress() {
     const offset = circumference - (progress * circumference);
     progressCircle.style.strokeDashoffset = offset; 
 }
+
+// Timer Core functions
+
+function startTimer() {
+    if (timer.isRunning)return;
+    
+    timer.isRunning = true;
+    timer.isPaused = false;
+
+    //UI update
+    startBtn.classList.add('hidden');
+    pauseBtn.classList.remove('hidden');
+
+    // add pulse annimation during active timer
+    document.querySelector('.timer-ring').style.animation = 'pulse 2s ease-in-out infinit';
+    
+
+    //start interval
+    timer.interval = setInterval(() => {
+        if (timer.seconds === 0) {
+            if (timer.minutes === 0) {
+                completeSession();
+                return;
+            }
+            timer.minutes--;
+            timer.seconds = 59;
+        } else {
+            timer.seconds--;
+        }
+        updateDisplay();
+        updateProgress();
+    }, 1000);
+}
+
+function updateDisplay() {
+    // format with zeros
+    const minutes = timer.minutes.toString().padStart(2, '0');
+    const seconds = timer.seconds.toString().padStart(2, '0');
+
+    timerDisplay.textContent = `${minutes} : ${seconds}`;
+    document.title = `${minutes} : ${seconds} - Pomodoro`;
+}
+
+function switchMode(mode) {
+    timer.mode = mode;
+    timer.minutes = settings[mode];
+    timer.seconds = 0;
+
+    // update active tab
+    tabBtn.forEach(btn => {
+        btn.classList.toggle('active', btn.dataset.mode === mode);
+    });
+
+    //update labels
+    const labels = {
+        pomodoro: 'Focus Time',
+        shortBreak: 'Short Break',
+        longBreak: "Long Break"
+    };
+    timerLabel.textContent = labels[mode];
+
+    // change colors
+    const colors = {
+        pomodoro: '#FF6B6B',
+        shortBreak: '#4ECDC4',
+        longBreak: '#45B7D1'
+    };
+    progressCircle.style.stroke = colors[mode]
+    
+    if (timer.isRunning) {
+        resetTimer();
+    } else {
+        updateDisplay();
+        updateProgress();
+    }
+}
